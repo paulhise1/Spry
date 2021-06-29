2eecd6a051be6edbc7568d65f781d1263069b108
PaulH <paulh@geocaching.com>
2021-06-29 13:52:40 -0700

    undoing changes from last commit so that I can push to a branch and make a PR for these changes.

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Quick/Quick.git",
-                 .exact("4.0.0")),
+                 .exact("3.1.2")),
         .package(url: "https://github.com/Quick/Nimble.git",
-                 .exact("9.2.0"))
+                 .exact("9.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
